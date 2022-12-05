acd04c27a252a5de4bfb25a329615abc34db773b
Gintare Dhavde <omkardhawde@MacBook-Air.local>
2022-12-05 09:13:34 +0000

    css added

--- src/Components/PokemonDetails.js
+++ src/Components/PokemonDetails.js
@@ -6,9 +6,9 @@ function PokemonDetails({name, height, weight, image}) {
 
   return (
     <>
-      <label index='Name'>Name: </label>{name}
-      <label index='Height'>Height: </label>{height}
-      <label index='Weight'>Weight: </label>{weight}
+      <label id='Name'>Name: </label>{name}
+      <label id='Height'>Height: </label>{height}
+      <label id='Weight'>Weight: </label>{weight}
       <img
         src={image}
         alt={name} />
